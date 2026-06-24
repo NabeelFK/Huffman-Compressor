921d194cd22e4afe001d9c50e6e512fc55dea74d
NabeelFK <nabeelfurqan8@gmail.com>
2026-06-24 14:58:03 -0600

    Added compression statistics

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <filesystem>
+#include <iomanip>
 
 #include "frequency.hpp"
 #include "huffman_tree.hpp"
@@ -55,7 +56,48 @@ int main(int argc, char *argv[])
             output_file
         );
 
-        std::cout << "Compression Complete.\n" << std::endl;
+        std::uintmax_t originalSize = std::filesystem::file_size(input_file);
+
+        std::uintmax_t compressedSize = std::filesystem::file_size(output_file);
+
+        size_t dotPos = input_file.find_last_of('.');
+
+        std::string extension;
+
+        if (dotPos == std::string::npos) {
+            extension = "";
+        } else {
+            extension = input_file.substr(dotPos + 1);
+        }
+
+        std::uintmax_t headerSize = 
+            sizeof(size_t) + 
+            extension.length() + 
+            (256 * sizeof(int)) + 
+            sizeof(std::size_t);
+
+        double savings = 0.0;
+
+        if (originalSize > 0) {
+            savings = 100.0 * 
+            (1.0 - 
+            static_cast<double>(compressedSize) / 
+            static_cast<double>(originalSize)
+            );
+        }
+
+        std::cout << "\nCompression Complete.\n";
+
+        std::cout << "\nCompression Statistics\n";
+        std::cout << "----------------------\n";
+
+        std::cout << "Original Size: " << originalSize << " bytes\n";
+
+        std::cout << "Compressed Size: " << compressedSize << " bytes\n";
+
+        std::cout << "Header Size: " << headerSize << " bytes\n";
+
+        std::cout << "Savings: " << std::fixed << std::setprecision(2) << savings << "%\n\n";
 
         deleteTree(root);
     }
